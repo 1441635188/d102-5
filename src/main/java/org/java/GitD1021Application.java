e40ce456c1bfbe7896170547d66cf4707a456c04
1441635188 <1441635188@qq.com>
2019-08-03 11:45:33 +0800

    第一次

---
+++ src/main/java/org/java/GitD1021Application.java
@@ -0,0 +1,13 @@
+package org.java;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitD1021Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GitD1021Application.class, args);
+    }
+
+}
---
+++ src/main/java/org/java/web/oneController.java
@@ -0,0 +1,14 @@
+package org.java.web;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class oneController {
+    @GetMapping("init")
+    public String init(){
+
+        return "你好";
+    }
+}
